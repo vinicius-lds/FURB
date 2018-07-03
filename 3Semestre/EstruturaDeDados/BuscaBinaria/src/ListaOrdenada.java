50937b911c2250f88725028382c7b1a2ce1817bf
Vinícius <vinicius.lds.br@gmail.com>
2018-07-03 14:04:23 -0300

    Algoritmos de busca binária e inserção ordenada corrigidos

--- 3Semestre/EstruturaDeDados/BuscaBinaria/src/ListaOrdenada.java
+++ 3Semestre/EstruturaDeDados/BuscaBinaria/src/ListaOrdenada.java
@@ -1,3 +1,4 @@
+
 /**
  * @author Vinícius Luis da Silva
  */
@@ -5,53 +6,50 @@ public class ListaOrdenada<T extends Comparable<T>> extends ListaAbstract<T> {
 
     @Override
     public void inserir(T valor) {
-        if(super.getTamanho() == 0) {
-            super.getInfo()[0] = valor;
-            return;
+        Object[] info = super.getInfo();
+        if (info.length == getTamanho()) {
+            redimensionar();
         }
-        Object[] aux = super.getInfo();
-        if(aux.length == super.getTamanho()) {
-            super.redimensionar();
-        }
-        for (int i = 0; i < super.getTamanho(); i++) {
-            if(valor.compareTo((T)aux[i]) < 0) {
-                for (int j = i + 1; j < super.getTamanho(); j++) {
-                    super.getInfo()[j] = (T)((aux[j + 1] != null) ? aux[j + 1] : null);
-                }
-                super.getInfo()[i] = valor;
+        int posicao = 0;
+        for (posicao = 0; posicao < getTamanho(); posicao++) {
+            if (((Comparable) info[posicao]).compareTo(valor) > 0) {
                 break;
             }
         }
-        super.setTamanho(super.getTamanho() + 1);
+        for (int i = getTamanho() - 1; i >= posicao; i--) {
+            info[i + 1] = info[i];
+        }
+        info[posicao] = valor;
+        setTamanho(getTamanho() + 1);
     }
-    
+
     @Override
     public int buscar(T valor) {
-        T[] info = super.getInfo();
-        
+        Object[] info = super.getInfo();
+
         int inicio = 0;
         int fim = super.getTamanho() - 1;
         int meio;
-        
-        while (inicio <= fim) {            
+
+        while (inicio <= fim) {
             meio = (inicio + fim) / 2;
-            int comparacao = valor.compareTo((T)info[meio]);
-            
-            if(comparacao < 0) {
-                fim = meio - 1;
+            int comparacao = ((Comparable) info[meio]).compareTo(valor);
+
+            if (comparacao > 0) {
+                    fim = meio - 1;
             } else {
-                if(comparacao > 0) {
-                    inicio = meio + 1;
+                if (comparacao < 0) {
+                inicio = meio + 1;
                 } else {
                     return meio;
                 }
             }
-            
+
         }
-        
+
         return -1;
     }
-    
+
     public static void main(String[] args) {
         ListaOrdenada<Integer> ints = new ListaOrdenada<>();
         ints.inserir(5);
@@ -60,6 +58,8 @@ public class ListaOrdenada<T extends Comparable<T>> extends ListaAbstract<T> {
         ints.inserir(6);
         ints.inserir(7);
         ints.inserir(9);
+        System.out.println(ints.toString());
+        System.out.println(ints.buscar(1));
     }
 
 }
